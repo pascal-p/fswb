1ce8fb5e62f3551b92ab4bccbf6e5139bfa7b189
Pascal P <lacsap_666@yahoo.fr>
2020-09-10 16:55:38 +1200

    add 1st comp, p2/2

--- C02/confusion/src/App.js
+++ C02/confusion/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes.js';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
   render() {
     return (
       <div>
@@ -14,7 +22,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }

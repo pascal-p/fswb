b52f003aab540fef50296af4b966f77d87c716b5
Pascal P <lacsap_666@yahoo.fr>
2020-09-11 18:09:58 +1200

    SPA - p1/2

--- C02/confusion/src/components/MainComponent.js
+++ C02/confusion/src/components/MainComponent.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Home from './HomeComponent';
-import Menu from './MenuComponent';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
+import Home from './HomeComponent';
+import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 // import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 
 class Main extends Component {
@@ -16,7 +20,9 @@ class Main extends Component {
     // state
     this.state = {
       dishes: DISHES,     // array of dishes
-      selectedDish: null  // only tracking the dishId, not the whole dish object
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -29,7 +35,9 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
 
@@ -39,6 +47,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />

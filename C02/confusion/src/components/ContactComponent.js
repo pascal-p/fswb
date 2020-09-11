b4fb2290d765ee36b2e5df47edd01d755fc65ad9
Pascal P <lacsap_666@yahoo.fr>
2020-09-11 18:46:43 +1200

    SPA - p2/2

--- C02/confusion/src/components/ContactComponent.js
+++ C02/confusion/src/components/ContactComponent.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 function Contact(props) {
   return(
     <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+          <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Contact Us</h3>
+          <hr />
+        </div>
+      </div>
+
       <div className="row row-content">
         <div className="col-12">
           <h3>Location Information</h3>
         </div>
+
         <div className="col-12 col-sm-4 offset-sm-1">
           <h5>Our Address</h5>
           <address>
@@ -18,9 +33,11 @@ function Contact(props) {
             <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
           </address>
         </div>
+
         <div className="col-12 col-sm-6 offset-sm-1">
           <h5>Map of our Location</h5>
         </div>
+
         <div className="col-12 col-sm-11 offset-sm-1">
           <div className="btn-group" role="group">
             <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>

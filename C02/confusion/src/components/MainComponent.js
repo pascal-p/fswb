047733d50d4042c295c7bf5b37fb7e5488e92092
Pascal P <lacsap_666@yahoo.fr>
2020-09-15 11:15:09 +1200

    redux thunk

--- C02/confusion/src/components/MainComponent.js
+++ C02/confusion/src/components/MainComponent.js
@@ -9,7 +9,7 @@ import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -25,26 +25,33 @@ const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId,
                                                                        rating,
                                                                        author,
-                                                                       comment))
-
+                                                                       comment)),
+  fetchDishes: () => { dispatch(fetchDishes()) }
 });
 
 class Main extends Component {
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
     const HomePage = () => {
       return(
-        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]} />
+        <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
 
     const DishWithId = ({match}) => {
-      const dish_id = parseInt(match.params.dishId,10);
+      const dish_id = parseInt(match.params.dishId, 10);
       return(
-        <DishDetail
-          dish={this.props.dishes.filter((dish) => dish.id === dish_id)[0]}
+        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === dish_id)[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
           comments={this.props.comments.filter((comment) => comment.dishId === dish_id)}
           addComment={this.props.addComment}
         />
--- C02/confusion/src/redux/ActionCreators.js
+++ C02/confusion/src/redux/ActionCreators.js
@@ -1,4 +1,30 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
+
+
+// action types:
+export const dishesLoading = () => ({
+  type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+  type: ActionTypes.DISHES_FAILED,
+  payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes
+});
+
+// a thunk:
+export const fetchDishes = () => (dispatch) => {
+  dispatch(dishesLoading(true));
+
+  setTimeout(() => {
+    dispatch(addDishes(DISHES));
+  }, 1000); // 1s is enough
+}
 
 export const addComment = (dishId, rating, author, comment) => ({
   type: ActionTypes.ADD_COMMENT,

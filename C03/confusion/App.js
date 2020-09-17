f8b9f6d883bda4f92c27390c2443176394f44261
Pascal P <lacsap_666@yahoo.fr>
2020-09-17 15:56:06 +1200

    Comp. part1

--- C03/confusion/App.js
+++ C03/confusion/App.js
@@ -1,22 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Adding some text here to check if it is updated on my tablet - Cool it is...</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return(<Main />);
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#D1C4E9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

9ec43435d99bacd7fce3c3414b681082fd9d0cbe
Pascal P <lacsap_666@yahoo.fr>
2020-09-20 11:03:12 +1200

    delete favorite

--- C03/confusion/components/FavoriteComponent.js
+++ C03/confusion/components/FavoriteComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity,
+         Animated } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -14,25 +17,52 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
+
 class Favorites extends Component {
-  /*
-  static navigationOptions = {
-    title: 'My Favorites'
-  };
-  */
 
   render() {
     const { navigate } = this.props.navigation;
 
+    const rightBtn = (progress, dragX, item) => {
+      const scale = dragX.interpolate({
+        inputRange: [-100, 0],
+        outputRange: [1, 0],
+        extrapolate: 'clamp'
+      });
+
+      return(
+        <React.Fragment>
+          <TouchableOpacity onPress={() => {
+              console.log("About to delete favorite dish: " + item.id);
+              this.props.deleteFavorite(item.id)
+            }
+          }>
+
+            <View style={{flex: 1, backgroundColor: '#dd2c00', justifyContent: 'center', alignItems: 'flex-end'}}>
+              <Animated.Text style={{color: '#ffffff', paddingHorizontal: 20, fontWeight:'bold', transform: [{scale}]}}>
+                Delete
+              </Animated.Text>
+            </View>
+          </TouchableOpacity>
+        </React.Fragment>
+      );
+    }
+
     const renderMenuItem = ({item, index}) => {
       return (
-        <ListItem key={index} onPress={() => navigate('Dishdetail', { dishId: item.id })}>
-          <Avatar source={{uri: baseUrl + item.image}} />
-          <ListItem.Content>
-            <ListItem.Title> {item.name} </ListItem.Title>
-            <ListItem.Subtitle> {item.description} </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
+        <Swipeable renderRightActions={(progress, dragX) => rightBtn(progress, dragX, item)}>
+          <ListItem key={index} onPress={() => navigate('Dishdetail', { dishId: item.id })}>
+            <Avatar source={{uri: baseUrl + item.image}} />
+            <ListItem.Content>
+              <ListItem.Title> {item.name} </ListItem.Title>
+              <ListItem.Subtitle> {item.description} </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+
+        </Swipeable>
       );
     };
 
@@ -53,4 +83,4 @@ class Favorites extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);

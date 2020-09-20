08cfd3dd14e9a18a6a39e4495d208fff29ca76ef
Pascal P <lacsap_666@yahoo.fr>
2020-09-21 11:42:54 +1200

    add social sharing

--- C03/confusion/components/DishdetailComponent.js
+++ C03/confusion/components/DishdetailComponent.js
@@ -1,7 +1,7 @@
 import React, { Component, useRef } from 'react';
 import { Text, View, ScrollView, FlatList,
          StyleSheet, Modal, Button,
-         Alert, PanResponder } from 'react-native';
+         Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input, } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
@@ -77,7 +77,17 @@ function RenderDish(props) {
       }
       return true;
     }
-  })
+  });
+
+  const shareDish = (title, message, url) => {
+    Share.share({
+      title: title,
+      message: `${title}: ${message} ${url}`,
+      url: url
+    }, {
+      dialogTitle: 'Share ' + title
+    })
+  }
 
   if (dish != null) {
     return(
@@ -95,7 +105,11 @@ function RenderDish(props) {
               onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()} />
 
             <Icon raised reverse name='pencil' type='font-awesome' color='#512DA8'
-              onPress={() => props.toggleModal() } />
+              onPress={() => props.toggleModal()} />
+
+            <Icon raised reverse name='share' type='font-awesome' color='#51D2A8' style={styles.cardItem}
+              onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
+
           </View>
         </Card>
       </Animatable.View>

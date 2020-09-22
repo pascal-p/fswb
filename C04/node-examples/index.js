41ccce090ef8b3fa0f1042329e326c6ce8c8c08f
Pascal P <lacsap_666@yahoo.fr>
2020-09-22 14:27:02 +1200

    node callbacks (cb) and errors

--- C04/node-examples/index.js
+++ C04/node-examples/index.js
@@ -1,23 +1,19 @@
-/*
-let rect = {
-  perimeter: (x, y) => (2*(x+y)),
-  area: (x, y) => (x*y)
-};
-*/
-
 let rect = require('./rectangle');
 
 solveRect = (l, b) => {
-  console.log("\nSolving for rectangle with l = " + l + " and b = " + b);
+  console.log(`\nSolving for rectangle with len=${l} and width=${b}`);
 
-  if (l <= 0 || b <= 0) {
-    console.log("Rectangle dimensions should be greater than zero:  l = "
-                + l + ",  and b = " + b);
-  }
-  else {
-    console.log("The area of the rectangle is " + rect.area(l,b));
-    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-  }
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR: ", err.message);
+    }
+    else {
+      console.log(`=> The area of the rectangle of len: ${l} and width: ${b} is: ${rectangle.area()}`);
+      console.log(`=> The perimeter of the rectangle of len: ${l} and width: ${b} is: ${rectangle.perimeter()}`);
+    }
+  });
+
+  console.log("This statement appears after the call to rect()");
 }
 
 solveRect(2,4);
--- C04/node-examples/rectangle.js
+++ C04/node-examples/rectangle.js
@@ -1,3 +1,15 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-
-exports.area = (x, y) => x * y;
+module.exports = (x, y, cb) => {
+  if (x <= 0 || y <= 0) {
+    let msg = `Rectangle dimensions should be greater than 0, len=${x} and width:${y}`;
+    setTimeout(() => cb(new Error(msg), null),
+               2000);
+  }
+  else {
+    setTimeout(() => cb(null,
+                        {
+                          perimeter: () => 2 * (x + y),
+                          area: () => x * y
+                        }),
+               2000);
+  }
+}

14ed26dede9758374d6fe17d2e61db818c76b11d
Pascal P <lacsap_666@yahoo.fr>
2020-09-22 15:38:13 +1200

    express serving static files

--- C04/node-express/index.js
+++ C04/node-express/index.js
@@ -1,18 +1,21 @@
 const express = require('express'),
       http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
 app.use((req, resp, next) => {
-  console.log(req.headers);
-  
   resp.statusCode = 200;
   resp.setHeader('Content-Type', 'text/html');
   resp.end('<html><body><h1>This is an Express Server</h1></body></html>');
-  
+
 });
 
 const server = http.createServer(app);

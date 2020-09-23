99bd3656b58fcc330cafb4cff5743d1aadecef78
Pascal P <lacsap_666@yahoo.fr>
2020-09-23 13:53:38 +1200

    mongoose part2/2

--- C04/node-mongoose/index.js
+++ C04/node-mongoose/index.js
@@ -5,30 +5,46 @@ const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url,
                                  { useUnifiedTopology: true,
+                                   useFindAndModify: false,  // https://mongoosejs.com/docs/deprecations.html#findandmodify
                                    useNewUrlParser: true }).catch(console.error);
 
 connect.then((db) => {
   console.log('Connected correctly to server');
 
-  let newDish = Dishes({
-    name: 'Uthappizza',
-    description: 'test'
-  });
+  Dishes.create({
+    name: 'Uthapizza',
+    description: 'Test'
+  })
+  .then((dish) => {
+    console.log("\nCreated new dish: ", dish);
+    return Dishes.findByIdAndUpdate(
+      dish._id,
+      { $set: {description: 'Updated test'} },
+      { new: true })
+      .exec();
+  })
+  .then((dish) => {
+    console.log("\nFind dish: ", dish);
 
-  newDish.save()
-    .then((dish) => {
-      console.log("Created new dish: ", dish);
-      return Dishes.find({});
-    })
-    .then((dishes) => {
-      console.log("Find all dishes: ", dishes);
-      return Dishes.deleteOne({});
-    })
-    .then(() => {
-      console.log("Deleted dish... Colsing db connection...");
-      return mongoose.connection.close();
-    })
-    .catch((err) => {
-      console.log(err);
+    dish.comments.push({
+      rating: 5,
+      comment: 'I\'m getting a sinking feeling!',
+      author: 'Leonardo di Carpaccio'
     });
+
+    return dish.save();
+  })
+  .then((dish) => {
+    console.log("\nAbout to delete dish: ", dish);
+
+    return Dishes.deleteOne({});
+  })
+  .then(() => {
+    console.log("\nDeleted dish... Closing db connection...");
+
+    return mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 });
--- C04/node-mongoose/models/dishes.js
+++ C04/node-mongoose/models/dishes.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema(
+  {
+    rating:  {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
+    },
+    comment:  {
+      type: String,
+      required: true
+    },
+    author:  {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  });
+
 const DishSchema = new Schema(
   {
     name: {
@@ -11,10 +32,11 @@ const DishSchema = new Schema(
     description: {
       type: String,
       required: true
-    }
+    },
+    comments:[commentSchema]
   },
   {
-    timestamps: true
+    timestamps: true // inject created_at, updated_at
   });
 
 let Dishes = mongoose.model('Dish', DishSchema);

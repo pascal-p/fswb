dcdaa31f8e52938d69bceac86c2e31b1ad41cf13
Pascal P <lacsap_666@yahoo.fr>
2020-09-23 12:04:21 +1200

    node mongodb ex3 - promises

--- C04/node-mongo/index.js
+++ C04/node-mongo/index.js
@@ -5,54 +5,48 @@ const dbOper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(
-  url,
-  { useUnifiedTopology: true },
-  (err, cli) => {
-    assert.equal(err, null);
+MongoClient.connect(url, { useUnifiedTopology: true })
+.then((cli) => {
+  console.log("DEBUG: connected to MongoSB server");
 
-    console.log("DEBUG: connected to MongoSB server");
+  const db = cli.db(dbname);
 
-    const db = cli.db(dbname);
+  // Create
+  dbOper.insertDocument(
+    db,
+    { name: "Vadonut", description: "Test"},
+    "dishes")
+    .then((res) => {
+      console.log("Insert Document:\n", res.ops);
 
-    // Nested structure of callbacks...
+      // Read
+      return dbOper.findDocuments(db, "dishes");
+    })
+    .then((docs) => {
+      console.log("Found Documents:\n", docs);
 
-    // Create
-    dbOper.insertDocument(
-      db,
-      { name: "Vadonut", description: "Test"},
-      "dishes", (res) => {
-        console.log(`Insert Document:\n ${res.ops}`);
+      // Update
+      return dbOper.updateDocument(
+        db,
+        { name: "Vadonut" },
+        { description: "Updated Test" }, "dishes");
+    })
+    .then((res) => {
+      console.log("==> Updated Document:\n", res.result);
 
-        // Read
-        dbOper.findDocuments(db, "dishes", (docs) => {
-          console.log("Found Documents:\n", docs);
+      // Read
+      return dbOper.findDocuments(db, "dishes");
+    })
+    .then((docs) => {
+      console.log("Found Updated Documents:\n", docs);
 
-          // Update
-          dbOper.updateDocument(
-            db,
-            { name: "Vadonut" },
-            { description: "Updated Test" }, "dishes",
-            (res) => {
-              console.log("==> Updated Document:\n", res.result);
-
-              // Read
-              dbOper.findDocuments(
-                db,
-                "dishes",
-                (docs) => {
-                  console.log("Found Updated Documents:\n", docs);
-
-                  // Delete
-                  db.dropCollection(
-                    "dishes",
-                    (res) => {
-                      console.log("Dropped Collection: ", res);
-                      cli.close();
-                    });
-                });
-            });
-        });
-      });
-
-});
+      // Delete
+      return db.dropCollection("dishes");
+    })
+    .then((res) => {
+      console.log("Dropped Collection: ", res);
+      return cli.close();
+    })
+    .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
--- C04/node-mongo/operations.js
+++ C04/node-mongo/operations.js
@@ -4,12 +4,7 @@ const assert = require('assert');
 exports.insertDocument = (db, doc, collection, cb) => {
   const coll = db.collection(collection);
 
-  coll.insertOne(doc, (err, res) => {
-    assert.equal(err, null);
-
-    console.log(`Inserted ${res.result.n} documents into the collection ${collection}`);
-    cb(res);
-  });
+  return coll.insertOne(doc);  // return a Promise
 };
 
 
@@ -17,31 +12,19 @@ exports.insertDocument = (db, doc, collection, cb) => {
 exports.findDocuments = (db, collection, cb) => {
   const coll = db.collection(collection);
 
-  coll.find({}).toArray((err, docs) => {
-    assert.equal(err, null);
-    cb(docs);
-  });
+  return coll.find({}).toArray();
 };
 
 // Update
 exports.updateDocument = (db, doc, update, collection, cb) => {
   const coll = db.collection(collection);
 
-  coll.updateOne(doc, { $set: update }, null, (err, res) => {
-    assert.equal(err, null);
-
-    console.log(`Updated the document with ${update}`);
-    cb(res);
-  });
+  return coll.updateOne(doc, { $set: update }, null);
 };
 
 // Delete
 exports.removeDocument = (db, doc, collection, cb) => {
   const coll = db.collection(collection);
 
-  coll.deleteOne(doc, (err, res) => {
-    assert.equal(err, null);
-    console.log(`Removed the document ${doc}`);
-    cb(res);
-  });
+  return coll.deleteOne(doc);
 };

ba6371e5dbd8f6f0690346d4bbe63b90d2a3f41d
Pascal P <lacsap_666@yahoo.fr>
2020-09-24 10:29:19 +1200

    review basic auth

--- C04/conFusionServer/app.js
+++ C04/conFusionServer/app.js
@@ -18,6 +18,41 @@ const connect = mongoose.connect(url,
                                    useFindAndModify: false,  // https://mongoosejs.com/docs/deprecations.html#findandmodify
                                    useNewUrlParser: true });
 
+const notAuthErr = new Error('You are not authenticated');
+const authHeader = ['WWW-Authenticate', 'Basic'];
+
+const setError = (resp) => {
+  let err = notAuthErr;
+  resp.setHeader(...authHeader);
+  err.status = 401;
+  return err;
+}
+
+const auth = (req, resp, next) => {
+  console.log("DEBUG: " + req.headers);
+
+  let authHeader = req.headers.authorization;
+  if (!authHeader) {
+    let err = setError(resp);
+    next(err);
+    return;
+  }
+
+  let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  let [user, pass] = auth; // [auth[0], auth[1]];
+  console.log(`DEBUG: auth user: ${user} / pass: ${pass}`);
+
+  if (user === 'admin' && pass === 'pass-8421') {
+    console.log('DEBUG: authorized');
+    next(); // authorized
+  }
+  else {
+    let err = setError(resp);
+    console.log('DEBUG: NOT authorized');
+    next(err);
+  }
+}
+
 connect.then((db) => {
   console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
@@ -32,13 +67,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, _resp, next) {

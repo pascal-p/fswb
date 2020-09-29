5e933c624bcfa9fe7a0d53778c7dc221e274b5dd
Pascal P <lacsap_666@yahoo.fr>
2020-09-30 09:16:28 +1300

    scrolling through Repos with useIterator

---
+++ Exo/ya-data/src/RepoMenu.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useIterator } from "./hooks";
+
+export default function RepoMenu({ repositories, onSelect = f => f }) {
+  const [{ name }, previous, next] = useIterator(repositories);
+
+  useEffect(() => {
+    if (!name) return;
+
+    onSelect(name);
+  }, [name, onSelect]);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <button onClick={previous}>&lt;</button>
+      <p><strong>{name}</strong></p>
+      <button onClick={next}>&gt;</button>
+    </div>
+  );
+}
---
+++ Exo/ya-data/src/hooks/index.js
@@ -0,0 +1,40 @@
+//
+// custom hooks
+//
+import { useState, useEffect, useCallback, useMemo } from "react";
+
+export function useFetch(uri) {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!uri) return;
+
+    fetch(uri)
+      .then(data => data.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [uri]);
+
+  return { loading, data, error };
+}
+
+export const useIterator = (items = [], initVal = 0) => {
+  const [ix, setIndex] = useState(initVal);
+
+  const prev = useCallback(() => {
+    if (ix === 0) return setIndex(items.length - 1);
+    setIndex(ix - 1);
+  }, [ix, items.length]);
+
+  const next = useCallback(() => {
+    if (ix === items.length - 1) return setIndex(0);
+    setIndex(ix + 1);
+  }, [ix, items.length]);
+
+  const item = useMemo(() => items[ix], [ix, items]);
+
+  return [item || items[0], prev, next];
+};
